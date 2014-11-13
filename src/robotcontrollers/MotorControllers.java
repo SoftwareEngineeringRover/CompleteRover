46724dd583a13b3cd89e0f615e21809d95b86978
junxinhe <hej77@students.rowan.edu>
2014-11-13 17:25:43 -0500

    first commit

---
+++ src/robotcontrollers/MotorControllers.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robotcontrollers;
+
+/**
+ *
+ * @author junxnhe
+ */
+public class MotorControllers extends RobotControllers{
+    String type="Motor";
+    int speed=0;
+    int degree;
+    int maxSpeed=-200;
+    
+    
+    public MotorControllers(int avoidIndex) {
+        super(avoidIndex);
+        super.searchForControllers();
+        if(isEmpty()){
+            System.out.println(type + " joystick connected!");
+        }else{
+            System.out.println("Unable to find extra joystick for "+type+" controller");
+        }
+    }
+    
+    @Override
+    public void getData(){
+        super.getData();
+        getSpeed();
+        getRotation();
+        //System.out.println(speed);
+        System.out.println(degree);
+    }
+    
+    private void getSpeed(){
+        speed=(int)(zAxis*maxSpeed);
+        //send to robot
+    }
+    
+    private void getRotation(){
+        degree=(int)Math.toDegrees(Math.atan2(-yAxis, xAxis));
+        if(degree<-90){
+            degree+=270;
+        }else{
+            degree-=90;
+        }
+        //send to robot
+    }
+}
